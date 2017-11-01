bd5f25be1c0155fce85cdb4502ae81d4a5fae58d
Onur Eker <yonureker@gmail.com>
2017-11-01 22:22:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name){
   kittens.push(name)
 }
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+}
